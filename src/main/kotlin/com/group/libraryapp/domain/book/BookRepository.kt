3d09cc4e64044aed85713db6a30133c1d82786c9
MyungHoon Ju <myunghoonju@gmail.com>
2026-02-17 19:46:01 +0900

    replace repository java to kt

---
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -0,0 +1,10 @@
+package com.group.libraryapp.domain.book
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+interface BookRepository : JpaRepository<Book, Long> {
+
+    //service layer uses Optional
+    fun findByName(name: String): Optional<Book>
+}
