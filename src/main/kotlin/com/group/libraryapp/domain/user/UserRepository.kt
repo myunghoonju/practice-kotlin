56879c83bb62c62f25c72c56c6be5cfed5f132fa
MyungHoon Ju <myunghoonju@gmail.com>
2026-02-17 22:50:16 +0900

    dto java to kt

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,9 +1,9 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.Optional
 
 interface UserRepository: JpaRepository<User, Long> {
 
-    fun findByName(name: String): Optional<User>
+    //spring support -> if not exists then return null
+    fun findByName(name: String): User?
 }
