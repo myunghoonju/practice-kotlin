4cfe22984d29d3a71b3ee5efeccdcf1894481567
MyungHoon Ju <myunghoonju@gmail.com>
2026-02-17 17:09:48 +0900

    user service test kt

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,8 +1,11 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -12,8 +15,13 @@ class UserServiceTest @Autowired constructor(
     private val userService: UserService,
     private val userRepository: UserRepository,
 ){
+    @AfterEach
+    fun cleanup(){
+        userRepository.deleteAll()
+    }
+
     @Test
-    fun saveUserTest() {
+    fun save_user_test() {
         //given
         var req = UserCreateRequest("myunghoon", null)
 
@@ -25,5 +33,38 @@ class UserServiceTest @Autowired constructor(
         assertThat(allUsers).hasSize(1)
         assertThat(allUsers[0].name).isEqualTo("myunghoon")
         assertThat(allUsers[0].age).isNull()
+    }
+
+    @Test
+    fun get_user_test() {
+        //given
+        userRepository.saveAll(listOf(
+            User("hoon", 19),
+            User("myung", 20),
+        ))
+
+        //when
+        val results = userService.getUsers()
+
+        //then
+        assertThat(results).hasSize(2)
+        assertThat(results).extracting("name")
+                           .containsExactlyInAnyOrder("hoon", "myung")
+        assertThat(results).extracting("age")
+                           .containsExactlyInAnyOrder(20, 19)
+    }
+
+    @Test
+    fun update_user_test() {
+        //given
+        val saved = userRepository.save(User("hoon", 19))
+        val req = UserUpdateRequest(saved.id, "ju")
+
+        //when
+        userService.updateUserName(req)
+
+        //then
+        val allUsers = userRepository.findAll()
+        assertThat(allUsers[0].name).isEqualTo("ju")
     }
 }
